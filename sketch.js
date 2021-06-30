5f13158bb9a0f9a9d36b729b7b51d1d025ac29ba
Saloni Sharma <68701930+saloni1966@users.noreply.github.com>
2021-06-30 09:14:39 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ var maxSnow=100;
 function preload() {
 bg=loadImage("snow1.jpg");
 gimg = loadImage("ground.PNG");
-runnerImg=loadAnimation("sc1.PNG","sc2.PNG","sc3.PNG","sc4.PNG","sc5.PNG","sc6.PNG","sc7.PNG","sc8.PNG","sc9.PNG","sc10.PNG","sc11.PNG","sc12.PNG")
+runnerImg=loadAnimation("sc1.PNG","sc2.PNG","sc3.PNG","sc4.PNG")
 }
 
 
